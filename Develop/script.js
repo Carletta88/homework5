f231f5475869eb8d9b33753fc0a1af75b2da18f9
Carletta Walls <carlettawalls88@gmail.com>
2021-04-28 22:59:00 -0400

    replaced some of the HTML with a different type of form, added more js script

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,28 @@
-//<script>
-// $( function() {
-//     $( "#datepicker" ).datepicker({
-//       showOn: "button-addon2",
-//       buttonImage: "images/calendar.gif",
-//       buttonImageOnly: true,
-//       buttonText: "Select date"
-//     });
-//   } );
-//   </script>
+var currentDayEl = $('#currentDay');
+var containerEl = $('.container');
+
+
+function displayToday() {
+    var todaysDate = moment().format('dddd, MMM Do');
+    currentDayEl.text(todaysDate);
+
+}
+
+setInterval(displayToday, 1000);
+
+function handleFormSubmit() {
+    
+    var calendarRow = $('input[info="schedule-input"]').val();
+
+    if (!calendarRow) {
+        console.log('what are you doing today')
+        return;
+    }
+
+    containerEl.append('<li>' + calendarRow + '</li>');
+
+    $('input[info="schedule-input"]').val();
+
+}
+
+handleFormSubmit();
