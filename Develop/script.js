257c2b4df93766aaa4e7e4479662d6acd2f1d145
Carletta Walls <carlettawalls88@gmail.com>
2021-04-28 21:02:58 -0400

    updated some js script and some html

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+//<script>
+// $( function() {
+//     $( "#datepicker" ).datepicker({
+//       showOn: "button-addon2",
+//       buttonImage: "images/calendar.gif",
+//       buttonImageOnly: true,
+//       buttonText: "Select date"
+//     });
+//   } );
+//   </script>
